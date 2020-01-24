52e1a5989fa7028b9c71a73ded9c2d5371a5199b
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-01-24 14:07:21 -0500

    almost finished

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+
+import SmurfsList from './smurfsList';
+import SmurfForm from './smurfsList';
+import Header from './header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+           <Header />
+
+   <div className = 'grid-2'>
+      <SmurfsList/>
+       <SmurfForm />
       </div>
+
+   </div>
     );
   }
 }
